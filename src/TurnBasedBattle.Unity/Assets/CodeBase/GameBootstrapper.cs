f78566d6d94f364c81d00b4602330a1b1fe49439
scrawach <scrawach@gmail.com>
2023-04-02 18:18:26 +0300

    upload unity blank project

---
+++ src/TurnBasedBattle.Unity/Assets/CodeBase/GameBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TurnBasedBattle.Model.Commands.Abstract;
+using TurnBasedBattle.Model.EventBus;
+using UnityEngine;
+
+namespace CodeBase
+{
+    public class GameBootstrapper : MonoBehaviour
+    {
+        private void Start()
+        {
+            var bus = new EventBus<ICommand>();
+            
+        }
+    }
+}
