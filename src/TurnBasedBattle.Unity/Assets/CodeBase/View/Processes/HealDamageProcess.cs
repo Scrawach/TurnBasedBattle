e044af2c3139fb3315b6995c3698e11915a2e5ff
scrawach <scrawach@gmail.com>
2023-04-11 13:36:59 +0300

    show heal bubble in heal damage process

--- src/TurnBasedBattle.Unity/Assets/CodeBase/View/Processes/HealDamageProcess.cs
+++ src/TurnBasedBattle.Unity/Assets/CodeBase/View/Processes/HealDamageProcess.cs
@@ -2,6 +2,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using CodeBase.View.Attributes;
 using CodeBase.View.Characters.Services;
+using CodeBase.View.Factory;
 using CodeBase.View.Processes.Abstract;
 using TurnBasedBattle.Model.Commands.Implementations;
 using TurnBasedBattle.Model.Core.Components;
@@ -13,22 +14,27 @@ namespace CodeBase.View.Processes
     public class HealDamageProcess : ViewProcess, IStartEventListener<HealDamage>
     {
         private readonly IGameObjectProvider _gameObjects;
+        private readonly IUIFactory _uiFactory;
 
-        public HealDamageProcess(IGameObjectProvider gameObjects) =>
+        public HealDamageProcess(IGameObjectProvider gameObjects, IUIFactory uiFactory)
+        {
             _gameObjects = gameObjects;
-        
+            _uiFactory = uiFactory;
+        }
+
         public void OnStart(HealDamage heal)
         {
             var health = heal.Target.Get<Health>();
             var current = health.Value;
             var total = health.Total;
             
-            Process(token => HealAsync(heal.Target.ToString(), current, total, token));
+            Process(token => HealAsync(heal.Target.ToString(), heal.Power, current, total, token));
         }
         
-        private async Task HealAsync(string id, int currentHealth, int totalHealth, CancellationToken token = default)
+        private async Task HealAsync(string id, int power, int currentHealth, int totalHealth, CancellationToken token = default)
         {
             var defender = _gameObjects[id];
+            _uiFactory.CreateHealBubble(defender.transform.position, power);
             await defender.HealthBar.UpdateValue(currentHealth, totalHealth, 1.0f, token);
         }
     }
